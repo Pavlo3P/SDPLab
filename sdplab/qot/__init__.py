486f117797e1fce6083812976e8028577945c7bd
Pavlo Pelikh <pavlopelikh@gmail.com>
2026-04-07 19:45:53 -0300

    Clean up cvxpy method

--- sdplab/qot/__init__.py
+++ sdplab/qot/__init__.py
@@ -2,6 +2,7 @@ from ._constraint_op import QOTConstraintOp
 from ._block_space import BlockMatrixSpace
 from .examples import generate_random_qot
 from ._linalg import compute_ptraces, kron_sum
+from ._cvxpy import solve_qot_dual
 
 __all__ = [
     "QOTConstraintOp",
@@ -11,4 +12,6 @@ __all__ = [
 
     "compute_ptraces",
     "kron_sum",
+
+    "solve_qot_dual",
 ]
--- sdplab/solvers/cvxpy/_interface.py
+++ sdplab/solvers/cvxpy/_interface.py
@@ -1,8 +1,6 @@
 from ...sdp import SDPDenseProblem, SDPProblem, SDPPrimal, SDPDual
-from ...qot import QOTProblem
 
 from ._sdp import solve_sdp_primal
-from ._qot import solve_qot_dual
 
 
 def run_cvxpy_solver(
@@ -13,7 +11,5 @@ def run_cvxpy_solver(
 ) -> tuple[SDPPrimal, SDPDual]:
     if isinstance(problem, SDPDenseProblem):
         return solve_sdp_primal(problem, solver, verbose, *args, **kwargs)
-    elif isinstance(problem, QOTProblem):
-        return solve_qot_dual(problem, solver, verbose, *args, **kwargs)
     else:
         raise ValueError('Unknown problem type: {}'.format(type(problem)))
