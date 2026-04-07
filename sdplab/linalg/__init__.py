940453f06bdc4337f2847bf8a3c12ca6543e1e32
Pavlo Pelikh <pavlopelikh@gmail.com>
2026-04-07 19:42:30 -0300

    Clean linalg folder

--- sdplab/linalg/__init__.py
+++ sdplab/linalg/__init__.py
@@ -1,6 +1,10 @@
-from ._qot import kron_sum, make_perm, compute_ptraces, _compute_ptraces
-
 from ._lanczos import stochastic_lanczos
 from ._power import power_method
 
 from ._misc import make_projector
+
+__all__ = [
+    "stochastic_lanczos",
+    "power_method",
+    "make_projector",
+]
