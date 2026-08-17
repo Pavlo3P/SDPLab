16a740ae3f911a99ecd68e3b2f9105e078ca4922
Pavlo Pelikh <pavlopelikh@gmail.com>
2026-08-17 14:32:41 -0400

    Add license header

--- sdplab/examples/qot/__init__.py
+++ sdplab/examples/qot/__init__.py
@@ -1,3 +1,17 @@
+# Copyright 2026 Pavlo Pelikh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 r"""Random quantum optimal-transport instances: a coupling
 :math:`\Gamma \in \operatorname{Herm}(d^N)` of minimal cost with prescribed
 one-body marginals :math:`\operatorname{Tr}^k[\Gamma] = \gamma_k`.
